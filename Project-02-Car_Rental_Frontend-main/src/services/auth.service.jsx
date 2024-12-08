7753073eb58dde40a1ec5da2661c56028871d596
Sankalp Gupta <sankalpgupta7860@gmail.com>
2024-12-08 14:00:27 +0530

    Update auth.service.jsx

--- Project-02-Car_Rental_Frontend-main/src/services/auth.service.jsx
+++ Project-02-Car_Rental_Frontend-main/src/services/auth.service.jsx
@@ -1,15 +1,32 @@
 import axios from 'axios';
+import { useLoading } from '../utils/LoadingContext';
 const API_URL = 'https://car-rental-backend-ogfw.onrender.com/api/auth';
 // const API_URL = import.meta.env.AUTH;
 
-const register = async (userData) => {
-  const response = await axios.post(`${API_URL}/register`, userData);
-  return response.data;
+const register = async (userData, showLoader, hideLoader) => {
+  try {
+    showLoader();
+    const response = await axios.post(`${API_URL}/register`, userData);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    hideLoader();
+  }
 };
 
-const login = async (email, password) => {
-  const response = await axios.post(`${API_URL}/login`, { email, password });
-  return response.data;
+const login = async (email, password, showLoader, hideLoader) => {
+  try {
+    showLoader();
+    const response = await axios.post(`${API_URL}/login`, { email, password });
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    throw error;
+  } finally {
+    hideLoader();
+  }
 };
 
 export { register, login };
