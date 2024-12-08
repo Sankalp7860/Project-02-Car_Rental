30731852d4b2729f1bf02f4906027c732ba58e14
Sankalp Gupta <sankalpgupta7860@gmail.com>
2024-12-08 13:56:40 +0530

    Create Loader.jsx

---
+++ Project-02-Car_Rental_Frontend-main/src/components/Loader.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Loader.css'; // Optional: Add custom styles for the loader.
+
+const Loader = () => (
+  <div className="loader-container">
+    <div className="loader"></div>
+  </div>
+);
+
+export default Loader;
