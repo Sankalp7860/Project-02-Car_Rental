63713274ff6aad222bac948d6e65be229149135f
Sankalp Gupta <sankalpgupta7860@gmail.com>
2024-12-08 14:01:18 +0530

    Update main.jsx

--- Project-02-Car_Rental_Frontend-main/src/main.jsx
+++ Project-02-Car_Rental_Frontend-main/src/main.jsx
@@ -2,11 +2,15 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import 'mdb-react-ui-kit/dist/css/mdb.min.css';
 import "@fortawesome/fontawesome-free/css/all.min.css";
+import { LoadingProvider } from './utils/LoadingContext';
+import Loader from './components/Loader';
 import App from './App.jsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <LoadingProvider>
+      <App />
+    </LoadingProvider>
   </React.StrictMode>,
 )
